0100b75d94d827a527b597995331ad0b962ceb45
Meke04 <luismekedev@gmail.com>
2026-08-28 16:42:31 -0300

    fix: associations

--- models/user.js
+++ models/user.js
@@ -14,6 +14,7 @@ module.exports = (sequelize, DataTypes) => {
       User.hasMany(models.Order, {
         foreignKey: 'userId'
       });
+
     }
 
   }
--- test-models.js
+++
@@ -1,9 +0,0 @@
-require('dotenv').config();
-
-const db = require('./models');
-
-console.log('User:', Object.keys(db.User.associations));
-console.log('Address:', Object.keys(db.Address.associations));
-console.log('Order:', Object.keys(db.Order.associations));
-console.log('Product:', Object.keys(db.Product.associations));
-console.log('Category:', Object.keys(db.Category.associations));
